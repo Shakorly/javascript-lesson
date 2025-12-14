d015f49bd152e9d1b84ab1562dddff9134a110cd
taiwo-shakiru <taiwoshakiru90@gmial.com>
2025-12-14 22:49:48 +0100

    update

---
+++ Todo-app/todo.js
@@ -0,0 +1,89 @@
+let taskList = [];
+
+const taskForm = document.getElementById('taskForm');
+const inputField = document.getElementById('task-input');
+
+
+taskForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputTask = inputField.value.trim();
+    
+    if(inputTask){
+        taskList.push({
+            id: Date.now(),
+            task: inputTask,
+            complete: false
+        } 
+       
+    )      
+    }
+
+
+    inputField.value = ''
+    renderTasks();
+})
+
+
+
+
+function renderTasks() {
+    const taskListContainer = document.getElementById('taskListContainer');
+    taskListContainer.innerHTML = '';
+
+    taskList.forEach(function(taskItem, id) {
+
+        const taskHTML = `
+            <div class="task-list"> 
+                <div class="check-box">
+                    <input type="checkbox" ${taskItem.complete ? 'checked' : ''} data-index="${id}" class="complete-checkbox"/>
+                </div>
+                <p class="task-text">${taskItem.task}</p>
+                <div>
+                    <div class="edit-text">
+                        <button class="edit-button">Edit</button>
+                        <button class="delete-button" id="deletebtn" data-id="${taskItem.id}">Delete</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        taskListContainer.insertAdjacentHTML('afterbegin', taskHTML);
+    });
+
+    eventBtn()
+}
+
+function eventBtn(){
+    const btns = document.querySelectorAll('.delete-button');
+    const completeBox = document.querySelectorAll('.complete-checkbox');
+
+    btns.forEach(btn =>{
+        btn.addEventListener('click', () => {
+            deleteBtn(Number(btn.dataset.id))
+        })
+    })
+
+    completeBox.forEach('btn', () =>{
+        btn.addEventListener('check', () => {
+            deleteBtn(Number(btn.dataset.id))
+        })
+    })
+}
+
+function deleteBtn(id){
+    taskList = taskList.filter(item => item.id !== id);
+    // taskList.splice(id, 1)
+    renderTasks();
+}
+
+function completeCheckbox(id){
+    const completeBox = taskList.find(item => item.id !== id)
+    if (completeBox){
+        backgrounColor: blue
+    }
+}
+
+
+
+
